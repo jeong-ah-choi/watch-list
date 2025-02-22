b61b82f0e50fb17a1c3a79c980fad8375cd9deef
JungAh Choi (최정아) <wjddk718@gmail.com>
2025-02-22 18:34:31 +0900

    Next.js 14, Supabase CRUD, Auth

---
+++ app/auth/callback/route.js
@@ -0,0 +1,18 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+
+  const { searchParams } = new URL(request.url);
+
+  const code = searchParams.get('code');
+
+  if (code) {
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(new URL('/watch-list', request.url));
+}
